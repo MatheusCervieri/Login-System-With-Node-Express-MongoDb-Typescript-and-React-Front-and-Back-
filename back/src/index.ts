6f81f149b0cf0268e458ba8d0d6ab2d18a4ab504
MatheusCervieri <Matheus Cervieri>
2023-03-27 11:02:29 -0300

    Save the basic information

---
+++ back/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+  
+app.listen(3001, () => {
+    console.log('Server is listening on port 3000');
+  });
